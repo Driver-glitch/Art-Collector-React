2c8403ad1fa45532211b8fbe50cfa9f41ae60063
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-09-20 18:08:24 -0400

    Finished project has 1 Warning

---
+++ build/precache-manifest.001d016e64b2fc2d8c2e3b810409f972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77efea0afa5df783e52a03baafdacb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7b77880fa758b29a7b",
+    "url": "/static/js/main.26c7a2de.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
